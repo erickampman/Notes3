926771d57659745c05e6f0420fb157a626acdb82
Eric Kampman <erickampman@yahoo.com>
2024-01-26 14:00:16 -0800

    Initial Commit

---
+++ Notes3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notes3
+//
+//  Created by Eric Kampman on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notes3/Notes3App.swift
@@ -0,0 +1,17 @@
+//
+//  Notes3App.swift
+//  Notes3
+//
+//  Created by Eric Kampman on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
